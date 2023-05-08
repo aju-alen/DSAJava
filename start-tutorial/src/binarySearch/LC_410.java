678f4866b66ef5885f266fd2f2402a628ce664e2
ajualen <alen@metamorphtech.com>
2023-05-08 10:16:16 +0400

    binary search leetcode question Day 4

---
+++ start-tutorial/src/binarySearch/LC_410.java
@@ -0,0 +1,10 @@
+package binarySearch;
+//https://leetcode.com/problems/split-array-largest-sum/
+public class LC_410 {
+    public static void main(String[] args){
+
+    }
+    static int splitArray(int[] nums, int k) {
+
+    }
+}
